87adc68228c0f837a7091d4d3d40e018c710467c
Karel Funda <karel.funda@gmail.com>
2024-02-28 22:11:44 +0100

    d

--- functions.py
+++ functions.py
@@ -29,7 +29,7 @@ def apply_user_fan_control_profile(idrac_login_string, hexadecimal_fan_speed):
 def retrieve_temperatures(idrac_login_string):
     data = subprocess.check_output(
         f"ipmitool -I {idrac_login_string} sdr type temperature", shell=True).decode('utf-8')
-    pring(f"data {data}")
+    print(f"data {data}")
     # Vyhledání všech teplot
     temperature_readings = re.findall(r'\|\s*(\d{2})\s*degrees C', data)
 
